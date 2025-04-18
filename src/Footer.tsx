f3fabbadc7739029c0000aa17d29c3e26e38a2c9
prashxant <notxdead0@gmail.com>
2025-04-18 11:45:59 +0530

    dist

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -30,7 +30,7 @@ const socialLinks = [
     ),
   },
   {
-    name: 'LinkedIn',
+    name: 'LinkedInn',
     href: 'https://www.linkedin.com/in/prashantshawrma',
     icon: (
       <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6">
